8b2cbcc9644fb1d35cd02a6bea995dc90f0752af
Miguel Ortiz <80050612+LuisMiguel-efe@users.noreply.github.com>
2024-10-31 14:08:03 -0500

    Creamos main del servidor
    Main donde se atienden solicitudes de registro y autenticacion

---
+++ main.py
@@ -0,0 +1,145 @@
+from fastapi import BackgroundTasks, FastAPI, Body, Form 
+from pydantic import BaseModel
+from typing import List
+import numpy as np
+from pymongo import MongoClient
+from tensorflow.keras.models import load_model  # type: ignore
+from model import compile_siamese_network
+from preprocess import hex_to_vector
+from sklearn.cluster import KMeans
+from datetime import datetime
+
+from dataset_train import hex_strings
+# Crear una instancia de FastAPI
+app = FastAPI()
+
+# Conexión a MongoDB
+client = MongoClient('mongodb+srv://luortiz:321@cluster0.n3xcu.mongodb.net/')
+db = client['bioaccess']
+collection_huellas = db['huellas']
+collection_accesos = db["accesos"]
+# Cargar el modelo Siamese CNN entrenado una sola vez
+
+# Definir el path de los pesos guardados
+model_path = 'siamese_model.keras'
+# Definir la forma de entrada (Tamaño fijo de los templates)
+input_shape = (512,)
+# Compilar y cargar el modelo
+model = compile_siamese_network(input_shape)
+model.load_weights(model_path)
+
+# Modelo de datos esperado por el servidor
+class TemplateData(BaseModel):
+    cedula: str
+    template1: str
+    template2: str
+    template3: str
+    template4: str
+    template5: str
+    template6: str
+    template7: str
+    template8: str
+    template9: str
+
+class FingerprintRequest(BaseModel):
+    templates: TemplateData
+
+# Ruta para registrar un nuevo usuario
+@app.post("/registrar_usuario")
+async def registrar_usuario(data: FingerprintRequest):
+    cedula = data.templates.cedula
+    fingerprints_hex = [
+        data.templates.template1,
+        data.templates.template2,
+        data.templates.template3,
+        data.templates.template4,
+        data.templates.template5,
+        data.templates.template6,
+        data.templates.template7,
+        data.templates.template8,
+        data.templates.template9
+    ]
+
+    # Convertir las cadenas hexadecimales en vectores numéricos
+    fingerprints_vectors = [hex_to_vector(fingerprint) for fingerprint in fingerprints_hex]
+
+    # Almacenar los vectores numéricos en lugar de las cadenas hexadecimales
+    fingerprints_matrix = np.array(fingerprints_vectors) 
+    # Guardar los vectores en la base de datos junto con la cédula
+    registro = {
+        "_id": cedula,
+        "templates": fingerprints_matrix.tolist(),  # Guardamos los vectores directamente
+    }
+    collection_huellas.insert_one(registro)
+
+    return {"status": "200 OK", "mensaje": "Usuario registrado exitosamente"}
+
+# Ruta de autenticación para comparar huellas con el modelo Siamese
+@app.post("/autenticar_huella")
+async def autenticar_huella(
+    background_tasks: BackgroundTasks,  # Añadimos BackgroundTasks para ejecutar en segundo plano
+    template: str = Form(...), 
+    porteria: str = Form(...),  # Número de la portería recibido en la solicitud
+):
+    
+    # Convertir la huella en un vector numérico
+    vector_huella = hex_to_vector(template).reshape(1, -1)
+    # registros = collection_huellas.find()
+    registros = list(collection_huellas.find({}))
+    if not registros:
+        # Si no hay registros, registrar el acceso fallido
+        background_tasks.add_task(registrar_acceso, None, porteria, False)
+        print("NO SE ENCONTRARON HUELLAS EN ESE CLUSTER")
+        return {"status": "No autenticado", "mensaje": "No se encontraron huellas en el cluster."}
+
+    # Variables para almacenar el mejor resultado
+    mejor_similitud = float('inf')  # Queremos minimizar la distancia
+    cedula_autenticada = None
+    print(mejor_similitud)
+    # Comparar la huella ingresada con todas las huellas almacenadas en ese cluster
+    for registro in registros:
+        stored_vectors = np.array(registro['templates'])  # Recuperar los vectores directamente
+        # Repetimos el vector de huella ingresado para hacer comparaciones por lotes
+        input_vectors = np.repeat(vector_huella, len(stored_vectors), axis=0)
+        
+        # Comparar todas las huellas en bloque con el modelo Siamese
+        distancias = model.predict([input_vectors, stored_vectors])
+
+        # Encontrar la distancia mínima
+        min_distancia = distancias.min()
+        if min_distancia < mejor_similitud:
+            mejor_similitud = min_distancia
+            cedula_autenticada = registro['_id']
+
+    # Definir un umbral para considerar una coincidencia (ajustar según rendimiento)
+    umbral_similitud = 0.1
+    print(mejor_similitud)
+    if mejor_similitud < umbral_similitud:
+        # Si autenticado, registrar el acceso exitoso
+        background_tasks.add_task(registrar_acceso, cedula_autenticada, porteria, True)
+        print("*** cedula autenticada: " + cedula_autenticada)
+        return{"valid": True}
+        #return {"status": "Autenticado", "Cedula": cedula_autenticada}
+    else:
+        # Si no autenticado, registrar el acceso fallido
+        background_tasks.add_task(registrar_acceso, None, porteria, False)
+        print("NO AUTENTICADO")
+        return{"valid": False}
+        # return {"status": "No autenticado"}
+    
+# Función asíncrona para registrar el acceso
+async def registrar_acceso(cedula, porteria, autenticado):
+    # Fecha y hora actual
+    fecha_acceso = datetime.now()
+    # Si la autenticación falló, la cédula es None
+    if not autenticado:
+        cedula = None
+    # Crear el registro de acceso
+    registro_acceso = {
+        "cedula": cedula,
+        "porteria": porteria,
+        "fecha_hora": fecha_acceso,
+        "autenticado": autenticado
+    }
+    # Insertar el registro en la colección "accesos"
+    collection_accesos.insert_one(registro_acceso)
